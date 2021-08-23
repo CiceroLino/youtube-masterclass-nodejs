f54a48ef1b0ad970814dbbfec22d7426ec2da6aa
Cicero Lino <cicerolinoeneto@gmail.com>
2021-08-23 01:07:46 -0300

    Add global function setInterval: loop

--- stats.js
+++ stats.js
@@ -1,20 +1,21 @@
 const os = require('os');
 
-// const freemem = os.freemem;
-// const totalmem = os.totalmem;
 const { freemem, totalmem } = os;
 
-const total = parseInt(totalmem() / 1024 / 1024);
-const mem = parseInt(freemem() / 1024 / 1024);
-// console.log(`${parseInt(freemem() / 1024 / 1024)} MB` ,totalmem());
-const percents = parseInt((mem / total) * 100);
+setInterval(() => {
 
-// console.log(mem, total, percents);
+    const total = parseInt(totalmem() / 1024 / 1024);
+    const mem = parseInt(freemem() / 1024 / 1024);
+    const percents = parseInt((mem / total) * 100);
 
-const stats = {
-    free: `${mem} MB`,
-    total: `${total} MB`,
-    usage: `${percents}%`
-};
-console.log('==== PC STATS ====')
-console.table(stats);
+    const stats = {
+        free: `${mem} MB`,
+        total: `${total} MB`,
+        usage: `${percents}%`
+    };
+
+    console.clear();
+    console.log('==== PC STATS ====')
+    console.table(stats);
+
+}, 1000);
