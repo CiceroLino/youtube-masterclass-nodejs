26e51b981cf6b7d5a7fcadf27011e28e86e3726a
Cicero Lino <cicerolinoeneto@gmail.com>
2021-08-27 23:41:11 -0300

    Add incomplete nodejs to project

--- http/server.js
+++ http/server.js
@@ -1,9 +1,19 @@
 const http = require('http')
+const fs = require('fs')
+const path = require('path')
 
 http.createServer((req, res) => {
-    if(req.url === '/')
-        return res.end('<h1>Home Page</h1>')
 
-    if(req.url === '/contato')
-        return res.end('<h1>Contato</h1>')
+    const file = req.url === '/' ? 'index.html' : req.url
+    const filePath = path.join(__dirname, 'public', file)
+
+    fs.readFile(
+        filePath,
+        (err, content) => {
+            if (err) throw err;
+ 
+            res.end(content)
+        }
+    )
+
 }).listen(5000, () => console.log('Server is running...'))
